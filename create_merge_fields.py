80a5b225b7ceebea2ae1a44192f430492745b0a4
Mike Killion <killion.kgs@gmail.com>
2015-06-30 11:28:54 -0500

    Uncomment lines for use in 10.0, update file paths

--- create_merge_fields.py
+++ create_merge_fields.py
@@ -4,9 +4,9 @@ start_time = strftime('%a, %d %b %Y %H:%M:%S', localtime())
 print start_time
 
 # Set paths and file names:
-output_dir = 'e:/field_boundaries'
+output_dir = 'c:/field_boundaries'
 output_gdb = 'fields.gdb'
-error_file = open('e:/field_boundaries/errors.txt', 'w')
+error_file = open('c:/field_boundaries/errors.txt', 'w')
 
 # Set scripting environment:
 import arcgisscripting
@@ -24,8 +24,8 @@ file = open(output_dir + '/field_list.txt')
 for line in file:
 	fieldname = line.strip()
 	try:
-		#gp.CopyRows('Database Connections/plss.sde/plss.' + fieldname, 'Database Connections/plss.sde/plss.TBL' + fieldname)
-		#fieldname = 'TBL' + fieldname
+		gp.CopyRows('Database Connections/plss.sde/plss.' + fieldname, 'Database Connections/plss.sde/plss.TBL' + fieldname)
+		fieldname = 'TBL' + fieldname
 		gp.AddJoin('plss_10_lyr', 'recnmbr', 'Database Connections/plss.sde/plss.' + fieldname, 'plss_recnmbr', 'keep_common')
 		gp.Dissolve('plss_10_lyr', output_dir + '/' + output_gdb + '/' + fieldname, 'plss.' + fieldname + '.field_kid', 'plss.' + fieldname + '.field_kid first;plss.' + fieldname + '.field_name first;plss.' + fieldname + '.produces_gas first;plss.' + fieldname + '.produces_oil first;plss.' + fieldname + '.status first;plss.' + fieldname + '.polydate first;plss.' + fieldname + '.cummulative_oil first;plss.' + fieldname + '.last_oil_production first;plss.' + fieldname + '.max_oil_wells first;plss.' + fieldname + '.last_oil_wells first;plss.' + fieldname + '.cummulative_gas first;plss.' + fieldname + '.last_gas_production first;plss.' + fieldname + '.max_gas_wells first;plss.' + fieldname + '.last_gas_wells first;plss.' + fieldname + '.avg_depth first;plss.' + fieldname + '.avg_depth_sealevel first', 'single_part')
 		gp.AddField(output_dir + '/' + output_gdb + '/' + fieldname, 'FIELD_KID', 'text', '#', '#', '25', 'FIELD_KID', 'nullable', 'non_required', '#')
@@ -35,7 +35,7 @@ for line in file:
 		gp.DeleteField(output_dir + '/' + output_gdb + '/' + fieldname, 'plss_' + fieldname + '_field_kid;first_plss_' + fieldname + '_field_kid;first_plss_' + fieldname + '_field_name;first_plss_' + fieldname + '_produces_gas;first_plss_' + fieldname + '_produces_oil;first_plss_' + fieldname + '_status;first_plss_' + fieldname + '_polydate;first_plss_' + fieldname + '_cummulative_oil;first_plss_' + fieldname + '_last_oil_production;first_plss_' + fieldname + '_max_oil_wells;first_plss_' + fieldname + '_last_oil_wells;first_plss_' + fieldname + '_cummulative_gas;first_plss_' + fieldname + '_last_gas_production;first_plss_' + fieldname + '_max_gas_wells;first_plss_' + fieldname + '_last_gas_wells;first_plss_' + fieldname + '_avg_depth;first_plss_' + fieldname + '_avg_depth_sealevel')
 		gp.RemoveJoin('plss_10_lyr', 'plss.' + fieldname)
 	except:
-		error_file = open('e:/field_boundaries/errors.txt', 'a')
+		error_file = open('c:/field_boundaries/errors.txt', 'a')
 		error_file.write(fieldname + '\n')
 		msg = gp.getmessages()
 		error_file.write(msg + '\n\n')
@@ -45,8 +45,8 @@ for line in file:
 
 ########## Merge fields ##########
 # Set workspace and output feature class name:
-gp.workspace = 'e:/field_boundaries/fields.gdb'
-out_fc = 'e:/field_boundaries/fields.gdb/fields'
+gp.workspace = 'c:/field_boundaries/fields.gdb'
+out_fc = 'c:/field_boundaries/fields.gdb/fields'
 
 # Create empty table to hold names of feature classes to be merged:
 input_fcs = gp.CreateObject('ValueTable')
